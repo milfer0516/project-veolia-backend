cbd25a6b8a5f67fe4bf2aee372e2735ab516cbf9
milfer0516 <milfer0516@gmail.com>
2024-09-23 15:40:24 -0500

    Variable de entorno a la conexio DB se modifica

--- src/config/connectionDB.js
+++ src/config/connectionDB.js
@@ -5,7 +5,9 @@ import { exit } from "node:process";
 // Replace with your MongoDB connection string
 export const connectionDB = async () => {
 	try {
-		const { connection } = await mongoose.connect(process.env.URL_CONNECTIONDB);
+		const { connection } = await mongoose.connect(
+			process.env.URL_CONNECTION_DB
+		);
 		const url = `${connection.host}:${connection.port}`;
 		console.log(`Conectado a MongoDB: ${url}`);
 	} catch (error) {
